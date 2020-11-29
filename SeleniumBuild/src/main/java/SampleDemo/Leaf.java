4763392b0661c1351ca5b582783a7d5e9c3ccbc4
karthikeyan kalyana chidambaram <kalyanachidambaram@gmail.com>
2020-11-29 11:10:56 +0530

    Base maven code

---
+++ SeleniumBuild/src/main/java/SampleDemo/Leaf.java
@@ -0,0 +1,26 @@
+package SampleDemo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Leaf {
+	
+	@Test
+	public void startLeaf() {	
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.close();
+	}
+
+}
